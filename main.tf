3bfb6305a6f69769e8ca826d5155db0e75aec595
lasertown <jasonju@gmail.com>
2021-11-21 16:10:51 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,3 +32,16 @@ offer = "sles-sap-15-sp3"
 sku = "gen2"
 _version = "latest"
 }
+
+module "client0" {
+source = "./modules/client"
+rg = module.rg0.rg
+region = module.network0.region
+subnet = module.network0.subnet
+vm_size = "Standard_E4s_v3"
+private_ip_address = 10.0.0.10
+publisher = "SUSE"
+offer = "sles-sap-15-sp3"
+sku = "gen2"
+_version = "latest"
+}
